5a14c874888be924f41805918cec2b00edd16cab
Michael Mastrucci <michael.mastrucci@gmail.com>
2022-10-02 13:04:11 -0400

    Set up omdb API and basic logging of data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
+
+const API_URL = 'http://www.omdbapi.com/?apikey=60ef3ecb'
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+useEffect(() => {
+  searchMovies()
+})
+
   return(
     <>
       <h1>App</h1>
